defffab151e2aaf05321411ea00b9a1d6cc56f46
takatsugu ishikawa <tissi0708@gmail.com>
2014-12-28 00:21:50 +0900

    imageminの追加

--- mysite/gulpfile.js
+++ mysite/gulpfile.js
@@ -1,9 +1,25 @@
 // src -> dist
 
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('hello', function () {
     console.log('hello world!');
 });
 
-gulp.task('default', ['hello']);
+gulp.task('img', function () {
+    gulp.src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('html', function () {
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('msg', ['html'], function () {
+    console.log('html copy done!');
+});
+
+gulp.task('default', ['msg', 'img']);
